9522b4a5f393d840edbbbdc869e2b3fc4f4edaeb
Trevor Incerti <tnincerti@gmail.com>
2019-11-12 22:52:32 -0500

    Updated abstract

--- code/aX. norms.R
+++ code/aX. norms.R
@@ -12,6 +12,8 @@ library(stargazer)
 # Data import - Gallup
 meta = read_excel("data/study_results.xlsx")
 load(file="data/meta.RData")
+gcb1 = read_excel("data/Global_Corruption_Barometer_2017_Global_Results.xlsx",
+                  sheet = "Q2_B", skip = 6)
 gcb2 = read_excel("data/Global_Corruption_Barometer_2017_Global_Results.xlsx",
                   sheet = "Q2_C", skip = 6)
 gcb5 = read_excel("data/Global_Corruption_Barometer_2017_Global_Results.xlsx",
@@ -24,6 +26,11 @@ gcb6 = read_excel("data/Global_Corruption_Barometer_2017_Global_Results.xlsx",
 ################################################################################
 
 # Clean gcb data
+gcb1 = gcb1 %>% 
+  select(Country, `NET NONE/ SOME`, `NET MOST/ALL`) %>%
+  rename(country = Country, rep_few = `NET NONE/ SOME`, rep_most = `NET MOST/ALL`)
+gcb1 = gcb1[-1,]
+
 gcb2 = gcb2 %>% 
   select(Country, `NET NONE/ SOME`, `NET MOST/ALL`) %>%
   rename(country = Country, gov_few = `NET NONE/ SOME`, gov_most = `NET MOST/ALL`)
@@ -31,7 +38,7 @@ gcb2 = gcb2[-1,]
 
 gcb5 = gcb5 %>% 
   select(Country, AGREE, DISAGREE) %>%
-  rename(country = Country, obligated_agree = AGREE, obligated_disagree = DISAGREE)
+  rename(country = Country, ordinary_agree = AGREE, ordinary_disagree = DISAGREE)
 gcb5 = gcb5[-1,]
 
 gcb6 = gcb6 %>% 
@@ -39,7 +46,8 @@ gcb6 = gcb6 %>%
   rename(country = Country, social_agree = AGREE, social_disagree = DISAGREE)
 gcb6 = gcb6[-1,]
 
-gcb = left_join(gcb2, gcb5, by = "country")
+gcb = left_join(gcb1, gcb2, by = "country")
+gcb = left_join(gcb, gcb5, by = "country")
 gcb = left_join(gcb, gcb6, by = "country")
 
 meta = left_join(meta, gcb, by = "country")
@@ -53,7 +61,20 @@ survey = meta %>%
   arrange(ate_vote) %>%
   mutate(author_reduced = reorder(author_reduced, -ate_vote))
 
-# Mixed effects model with norms moderator
-me_mod = rma(yi = ate_vote, sei = se_vote, mods = social_agree, data = survey)
+# Mixed effects model with norms moderators
+me_mod_rep = rma(yi = ate_vote, sei = se_vote, 
+                    mods = rep_most, data = survey)
 
-predict(me_mod, newmods = cbind(seq(from = 0, to = 1, by = .1)), addx = TRUE)
+me_mod_gov = rma(yi = ate_vote, sei = se_vote, 
+                    mods = gov_most, data = survey)
+
+me_mod_social = rma(yi = ate_vote, sei = se_vote, 
+                    mods = social_agree, data = survey)
+
+me_mod_ordinary = rma(yi = ate_vote, sei = se_vote, 
+                       mods = ordinary_agree, data = survey)
+
+predict(me_mod_rep, newmods = cbind(seq(from = 0, to = 1, by = .1)), addx = TRUE)
+predict(me_mod_gov, newmods = cbind(seq(from = 0, to = 1, by = .1)), addx = TRUE)
+predict(me_mod_social, newmods = cbind(seq(from = 0, to = 1, by = .1)), addx = TRUE)
+predict(me_mod_ordinary, newmods = cbind(seq(from = 0, to = 1, by = .1)), addx = TRUE)
